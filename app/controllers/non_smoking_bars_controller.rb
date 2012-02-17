7206ef0850ff5d672367db368843fa66a20f40bd
Ivana Vasilj <ivasilj@gmail.com>
2012-02-18 00:18:37 +0100

    corrected properties in list

--- app/controllers/non_smoking_bars_controller.rb
+++ app/controllers/non_smoking_bars_controller.rb
@@ -7,6 +7,14 @@ class NonSmokingBarsController < ApplicationController
   def index
     @non_smoking_bars = FsqBar.all
 
+    @non_smoking_bars.each { |fsq_bar|
+      if NonSmokingBar.exists? conditions: {id:fsq_bar.id}
+        edited = NonSmokingBar.find fsq_bar.id
+        fsq_bar.name = edited.name
+        fsq_bar.address = edited.address
+      end
+    }
+
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @non_smoking_bars }
