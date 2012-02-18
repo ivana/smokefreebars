cfc4abf99530f83193f1c67a698116b36b78675a
Ivana Vasilj <ivasilj@gmail.com>
2012-02-18 14:30:00 +0100

    remove relational db files

--- db/migrate/20120124211832_add_auth_token_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddAuthTokenToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :auth_token, :string
-
-  end
-end
-
-# User.all.each { |u| u.auth_token = SecureRandom.urlsafe_base64; u.save }
