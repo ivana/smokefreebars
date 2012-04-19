6247fc2aea4c2d34bc04ce3e4f948846ed5039bc
Ivana Vasilj <ivasilj@gmail.com>
2012-04-19 14:29:22 +0200

    index now returns mongoid bars, synchronized with foursquare list

--- app/controllers/non_smoking_bars_controller.rb
+++ app/controllers/non_smoking_bars_controller.rb
@@ -3,15 +3,8 @@ class NonSmokingBarsController < ApplicationController
   # GET /non_smoking_bars
   # GET /non_smoking_bars.json
   def index
-    @non_smoking_bars = FsqBar.all
-
-    @non_smoking_bars.each { |fsq_bar|
-      if NonSmokingBar.exists? conditions: {id:fsq_bar.id}
-        edited = NonSmokingBar.find fsq_bar.id
-        fsq_bar.name = edited.name
-        fsq_bar.address = edited.address
-      end
-    }
+    sync_with_foursquare
+    @non_smoking_bars = NonSmokingBar.all
 
     respond_to do |format|
       format.html # index.html.erb
@@ -34,4 +27,25 @@ class NonSmokingBarsController < ApplicationController
     end
   end
 
+
+  private
+
+  # synchronizes bars data in mongodb with the foursquare list
+  def sync_with_foursquare
+    # 1. fetch foursquare list and mongo data
+    fsq_bars = FsqBar.all
+    fsq_ids = fsq_bars.map &:id
+    mongo_ids = NonSmokingBar.all.map &:fsq_id
+
+    # 2. delete bars missing in foursquare list from mongo
+    removed_ids = mongo_ids - fsq_ids
+    NonSmokingBar.where(:fsq_id.in => removed_ids).delete_all
+
+    # 3. save new bars in foursquare list to mongo
+    added_ids = fsq_ids - mongo_ids # example: ["4d5d156d9895b1f76807ec0f", "4c6946383bad2d7ff65cafee"]
+    added_ids.each { |fsq_id|
+      new_bar = fsq_bars.select { |bar| bar.id == fsq_id }.first
+      NonSmokingBar.create! fsq_id: new_bar.id, name: new_bar.name, address: new_bar.address, coords: { lat: new_bar.lat, lng: new_bar.lng }
+    }
+  end
 end
